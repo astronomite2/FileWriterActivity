a2581ffae74c29020b0c36905a174b03b317decb
Declan <dmurphy1@hwemail.com>
2024-09-10 12:57:41 -0700

    Added .gitIgnore and Secret File Writer Code in MyFileWriter
    2 password file writers and .gitignore for txt files and and dat files added.

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -4,44 +4,19 @@ import java.nio.charset.StandardCharsets;
 
 public class MyFileWriter {
     public static void main(String[] args) {
-        String data = "Hello, World!";
-        String fileName1 = "example.txt";
-        String fileName2 = "example2.txt";
-        String fileName3 = "example3.txt";
-        String fileName4 = "example4.txt";
-        String fileName5 = "example5.txt";
+        String passKey = "MarvelJesusD33DP001";
+        String confidentialData = ":OOOO";
+        String disneyPlusPass = ".disneyPlusPass.txt";
+        String topSecret = ".classified/confidential.dat";
 
         // 1. Using FileWriter
-        try (FileWriter writer = new FileWriter(fileName1)) {
-            writer.write(data);
+        try (FileWriter writer = new FileWriter(disneyPlusPass)) {
+            writer.write(passKey);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        // 2. Using BufferedWriter
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
-            bufferedWriter.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 3. Using FileOutputStream
-        try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
-            outputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 4. Using BufferedOutputStream
-        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
-            bufferedOutputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 5. Using Files (java.nio.file)
-        try {
-            Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
+        try (FileWriter writer = new FileWriter(topSecret)) {
+            writer.write(confidentialData);
         } catch (IOException e) {
             e.printStackTrace();
         }
